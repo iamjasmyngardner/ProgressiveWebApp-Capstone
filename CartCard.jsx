1fd6d9f668e747a8adcd67f70f94dc87c0913625
iamjasmyngardner <gardner.jasmyn@yahoo.com>
2025-05-11 12:33:16 -0400

    EZ Tech Movie CartCard.jsx
    Shopping cart tile

---
+++ CartCard.jsx
@@ -0,0 +1,14 @@
+
+function CartCard (){
+
+    return(
+        <div className="cart card">
+            <img className="cart-card-image" src="https://media.istockphoto.com/id/1393359760/photo/basket-or-shopping-cart-icon-on-blue-background-online-shopping-e-commerce-concept-3d.jpg?s=612x612&w=0&k=20&c=1BVuytlIgWIpoTjh-GaN1IVwi4Cp0C-7l1IJMDf23aE=" alt="Online shopping" />
+            <h2 className="cart-card-title">Cart</h2>
+            <p className="cart-card-text">Click here to view shopping cart</p>
+        </div>
+    );
+
+}
+
+export default CartCard
