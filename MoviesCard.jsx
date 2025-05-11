8b16fc7aa17684b141279bacd98eed135a18786b
iamjasmyngardner <gardner.jasmyn@yahoo.com>
2025-05-11 12:39:07 -0400

    EZ Tech Movie MoviesCard.jsx
    Movie Card tile component

---
+++ MoviesCard.jsx
@@ -0,0 +1,13 @@
+
+function MoviesCard (){
+    return(
+        <div className="movies card">
+            <img className="movies-card-image" src="https://media.istockphoto.com/id/1318278635/photo/popcorn-close-up-open-air-cinema-concept.jpg?s=612x612&w=0&k=20&c=PAgZvQtUk1cG9Yz1XxLxkfzWwSC4zSDxpeIQiYdxVCM=" alt="Popcorn and movie night" />
+            <h2 className="movies-card-title">Movie Search</h2>
+            <p className="movies-card-text">Click here for find movie</p>
+        </div>
+    );
+
+}
+
+export default MoviesCard
