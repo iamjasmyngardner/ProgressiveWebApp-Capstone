764845890eb952e378114292cea353d3addd5e1f
iamjasmyngardner <gardner.jasmyn@yahoo.com>
2025-05-11 12:39:40 -0400

    EZ Tech Movie StreamCard.jsx
    Stream Card tile component

---
+++ StreamCard.jsx
@@ -0,0 +1,13 @@
+
+function StreamCard(){
+    return(
+        <div className="stream card">
+            <img className="stream-card-image" src="https://media.istockphoto.com/id/1263618654/vector/colorful-mockup-template-on-gray-background-with-light-blue-gradient-life-stream-video.jpg?s=612x612&w=0&k=20&c=U-eq4ngVRLpjP0zhoOU8wOArm8L_DHu1jwC7CdErskM=" alt="Streaming on laptop" />
+            <h2 className="stream-card-title">Stream List</h2>
+            <p className="stream-card-text">Click here for saved stream list to watch in future</p>
+
+        </div>
+    );
+}
+
+export default StreamCard
