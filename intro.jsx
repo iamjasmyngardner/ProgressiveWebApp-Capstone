d00237bd0676d55ee845ae78d7138d32ff07dc4b
iamjasmyngardner <gardner.jasmyn@yahoo.com>
2025-05-11 12:37:09 -0400

    EZ Tech Movie intro.jsx
    Introduction function, placeholder for small paragraph introducing app

---
+++ intro.jsx
@@ -0,0 +1,10 @@
+
+function Intro() {
+    return(
+        <section className="intro">
+            <p>This is EZ Tech Movie. Your place to stream any movie, any time! Save movies to Stream List to watch later or rewatch favorites.</p>
+        </section>
+    );
+}
+
+export default Intro; 
