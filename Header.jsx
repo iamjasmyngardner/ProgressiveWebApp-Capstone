4c79584f5e457d5221920c862c48e48ee8b8c50c
iamjasmyngardner <gardner.jasmyn@yahoo.com>
2025-05-11 12:35:28 -0400

    EZ Tech Movie Header.jsx
    Header function, imports Header styling

---
+++ Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function Header() {
+
+    return(
+        <header>
+            <h1>EZTech Movie</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
